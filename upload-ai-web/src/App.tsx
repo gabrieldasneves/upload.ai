9252c9a52ad4772f80036ceeab75dbc18c9ad8c9
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-13 10:03:06 +0900

    finish Header interface and icons insertion

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -1,3 +1,21 @@
+import { Separator } from "./components/ui/separator";
+import { Button } from "./components/ui/button";
+import { Github } from "lucide-react";
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>;
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Developped with 💜 on NLW event
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant="outline">
+            <Github className="w-4 h-4 mr-2" /> Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
