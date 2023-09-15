3a8140dde2c3c6151b4b2e44efacad544d71f56a
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-15 10:02:08 +0900

    upload video routes

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -1,10 +1,12 @@
 import { fastify } from "fastify";
+import { prisma } from "./lib/prisma";
+import { getAllPromptSRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "hello world";
-});
+app.register(getAllPromptSRoute);
+app.register(uploadVideoRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running");
