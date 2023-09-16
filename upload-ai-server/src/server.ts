05487776bd66c217525ccb9dda2545081c039477
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2023-09-16 17:31:24 +0900

    create-transcription route

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -2,11 +2,13 @@ import { fastify } from "fastify";
 import { prisma } from "./lib/prisma";
 import { getAllPromptSRoute } from "./routes/get-all-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify();
 
 app.register(getAllPromptSRoute);
 app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("server running");
